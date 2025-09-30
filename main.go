fdf4c4aa17a162530f66c7b3a730d92e58c313c7
crspradlin <github@crspradlin.org>
2025-09-29 22:27:30 -0400

    Init Commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  //_ "github.com/mattn/go-sqlite3"
+  "github.com/charmbracelet/log"
+
+  // "html/template"
+  // "net/http"
+  // "fmt"
+  // "errors"
+  // "strings"
+)
+
+func main() {
+  log.Info("Testing Compile and Runtime...")
+}
+
+
