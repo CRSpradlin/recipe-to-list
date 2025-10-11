2e8d07806e45a37111d239fd2f08c615c68721f2
crspradlin <github@crspradlin.org>
2025-10-10 22:22:27 -0400

    Added initial recipe create and update db logic

--- main.go
+++ main.go
@@ -11,17 +11,22 @@ import (
 
 	// "fmt"
 	"errors"
-	// "strings"
+	"strings"
 )
 
 const SQLITE_FILE_LOC = "./.data/"
 const SQLITE_FILE_NAME = "db.sqlite"
 const SQLITE_DB_SCHEMA = `
-	create table if not exists recipe (
+	create table if not exists recipes (
 		id integer not null primary key,
 		ingredients text not null
 	);
 `
+
+type Recipe struct {
+	id          *int64
+	ingredients []string
+}
 
 var db *sql.DB
 
@@ -80,6 +85,45 @@ func initSqliteDB() (*sql.DB, error) {
 	}
 
 	return initDB, nil
+}
+
+func handleRecipeUpdate(recipe Recipe) (Recipe, error) {
+	tx, dbBeginErr := db.Begin()
+	if dbBeginErr != nil {
+		return recipe, errors.Join(dbBeginErr, errors.New("Could not start database tx for recipe create/update."))
+	}
+
+	if recipe.id == nil {
+		stmt, stmtPrepareError := tx.Prepare("insert into recipes(ingredients) values(?)")
+		if stmtPrepareError != nil {
+			return recipe, errors.Join(stmtPrepareError, errors.New("Could not prepare statement for recipe create."))
+		}
+
+		result, stmtExecErr := stmt.Exec(strings.Join(recipe.ingredients, "|"))
+		if stmtExecErr != nil {
+			return recipe, errors.Join(stmtExecErr, errors.New("Could not execute the create recipe query statement."))
+		}
+
+		createdId, sqlResultErr := result.LastInsertId()
+		if sqlResultErr != nil {
+			return recipe, errors.Join(sqlResultErr, errors.New("Could not parse create recipe sql result and inserted id value."))
+		}
+
+		recipe.id = &createdId
+
+	} else {
+		stmt, stmtPrepareError := tx.Prepare("update recipes set ingredients=? where id=?")
+		if stmtPrepareError != nil {
+			return recipe, errors.Join(stmtPrepareError, errors.New("Could not prepare statement for recipe update."))
+		}
+
+		_, stmtExecErr := stmt.Exec(strings.Join(recipe.ingredients, "|"), recipe.id)
+		if stmtExecErr != nil {
+			return recipe, errors.Join(stmtExecErr, errors.New("Could not execute the recipe update query statement."))
+		}
+	}
+
+	return recipe, nil
 }
 
 func handleRootRequest(rw http.ResponseWriter, req *http.Request) {
